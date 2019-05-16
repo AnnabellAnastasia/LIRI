140093fb82fdf997de314556b2baba5e838fa15b
AnnabellAnastasia <oav2008@gmail.com>
2019-05-16 14:41:17 -0400

    spotify

--- liri.js
+++ liri.js
@@ -21,15 +21,27 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 }
  
-var getMeSpotify = function(SongName){
-    spotify.search({ type: 'track', query: songName }, 
+var getArtistName = function(artist){
+    return artist.name;
+}
+
+var getMeSpotify = function(songName) {
+    spotify.search( { type: 'track', query: songName }, 
     function(err, data) {
         if ( err ) {
             console.log('Error occurred: ' + err);
             return;
         }
-     
-    console.log(data.tracks.items[0])
+        var songs = data.tracks.items;
+        for(var i=0; i<songs.length; i++){
+            console.log(i);
+            console.log('artist(s): '+ songs[i].artists.map(
+                getArtistName));
+            console.log('song name: '+ songs[i].name);
+            console.log('preview song: '+ songs[i].preview_url);
+            console.log('album: '+ songs[i].album.name);
+            console.log('----------------------------------------')
+            }
     });
 }
 
@@ -39,6 +51,9 @@ var pick = function(caseData, functionData){
     switch(caseData){
         case 'my-tweets':
         getMyTweets();
+        break;
+        case 'spotify-this-song':
+        getMeSpotify(functionData);
         break;
         default:
         console.log('Liri does not know that');
