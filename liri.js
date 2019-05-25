ad01ad3ff612c033e2936eb3c8ac0fd0dc3fa6a9
AnnabellAnastasia <oav2008@gmail.com>
2019-05-24 23:05:05 -0400

    tweets

--- liri.js
+++ liri.js
@@ -5,69 +5,73 @@ var Twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 
-var getMyTweets = function (){
+var getMyTweets = function () {
 
-var client = new Twitter(keys.twitterKeys);
- 
-var params = {screen_name: 'Anna75893882'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    // console.log(tweets);
-    for(var i=0; i<tweets.length; i++){
-        console.log(tweets[i].created_at);
-        console.log('');
-        console.log(tweets[i].text);
-    }
-  }
-});
-}
- 
-var getArtistName = function(artist){
-    return artist.name;
-}
+    var client = new Twitter(keys.twitterKeys);
+    console.log(keys.twitterKeys);
 
-var getMeSpotify = function(songName) {
-    spotify.search( { type: 'track', query: songName }, 
-    function(err, data) {
-        if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
-        var songs = data.tracks.items;
-        for(var i=0; i<songs.length; i++){
-            console.log(i);
-            console.log('artist(s): '+ songs[i].artists.map(
-                getArtistName));
-            console.log('song name: '+ songs[i].name);
-            console.log('preview song: '+ songs[i].preview_url);
-            console.log('album: '+ songs[i].album.name);
-            console.log('----------------------------------------')
+    var params = { screen_name: 'Anna75893882' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            // console.log(tweets);
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log('');
+                console.log(tweets[i].text);
             }
+        }
     });
 }
 
-var getMeMovie = function(movieName){
-    request('http://www.omdbapi.com/?t' + movieName + '&y=&plot=short&r=json', 
-    function (error, response, body) {
-    if (!error && response.statusCode == 200){
-        console.log(body);
-    } 
-    })
-}
-     
+// var getArtistName = function(artist){
+//     return artist.name;
+// }
 
-var pick = function(caseData, functionData){
-    switch(caseData){
+// var getMeSpotify = function(songName) {
+//     spotify.search( { type: 'track', query: songName }, 
+//     function(err, data) {
+//         if ( err ) {
+//             console.log('Error occurred: ' + err);
+//             return;
+//         }
+//         console.log(data)
+//         var songs = data.tracks.items;
+//         for(var i=0; i<songs.length; i++){
+//             console.log(i);
+//             console.log('artist(s): '+ songs[i].artists.map(
+//                 getArtistName));
+//             console.log('song name: '+ songs[i].name);
+//             console.log('preview song: '+ songs[i].preview_url);
+//             console.log('album: '+ songs[i].album.name);
+//             console.log('----------------------------------------')
+//             }
+//     });
+// }
+
+// var getMeMovie = function(movieName){
+//     request('http://www.omdbapi.com/?t' + movieName + '&y=&plot=short&r=json&apikey=trilogy', 
+//     function (error, response, body) {
+//     if (!error && response.statusCode == 200){
+//         console.log(body);
+//     } 
+//     console.log(response)
+//     })
+// }
+
+
+var pick = function (caseData, functionData) {
+    switch (caseData) {
         case 'my-tweets':
-        getMyTweets();
-        break;
-        case 'spotify-this-song':
-        getMeSpotify(functionData);
-        break;
-        case 'movie-this':
-        getMeMovie(functionData);
+            getMyTweets();
+            break;
+        //         case 'spotify-this-song':
+        //         getMeSpotify(functionData);
+        //         break;
+        //         case 'movie-this':
+        //         getMeMovie(functionData);
+        //         break;
         default:
-        console.log('Liri does not know that');
+            console.log('Liri does not know that');
     }
 }
 
