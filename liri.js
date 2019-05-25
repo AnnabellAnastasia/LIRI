e86f53e2b2f05472ce18243031ead274e4f3e346
AnnabellAnastasia <oav2008@gmail.com>
2019-05-25 00:40:10 -0400

    movie

--- liri.js
+++ liri.js
@@ -1,8 +1,7 @@
-// require("dotenv").config();
-
-
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
-
+var Movie = require
 
 var pick = function (caseData, functionData) {
     switch (caseData) {
@@ -47,78 +46,78 @@ function getMeSpotify() {
             return console.log('Error occurred: ' + err);
         }
 
-        // returning artist name
-        console.log(data.tracks.items[0].artists[0].name);
+        // // returning artist name
+        // console.log(data.tracks.items[0].artists[0].name);
 
-        // returning song's name 
-        console.log(data.tracks.items[0].name);
+        // // returning song's name 
+        // console.log(data.tracks.items[0].name);
 
-        // returning preview link of the song from Spotify
-        console.log(data.tracks.items[0].preview_url);
+        // // returning preview link of the song from Spotify
+        // console.log(data.tracks.items[0].preview_url);
 
-        // returning album that the song is from
-        console.log(data.tracks.items[0].album.name);
+        // // returning album that the song is from
+        // console.log(data.tracks.items[0].album.name);
     });
 };
 
-function getMeMovie() {
+// function getMeMovie() {
 
-    // var omdb = require('omdb');
+//     // var omdb = require('omdb');
 
-    var request = require('request');
-    request('http://www.omdbapi.com/?apikey=trilogy&plot=full&tomatoes=true&r=json&t=' + value, function (error, response, body) {
-        //   console.log('error:', error); // Print the error if one occurred
-        //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     var request = require('request');
+//     request('http://www.omdbapi.com/?apikey=trilogy&plot=full&tomatoes=true&r=json&t=' + value, function (error, response, body) {
+//         //   console.log('error:', error); // Print the error if one occurred
+//         //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-        const obj = JSON.parse(body);
+//         const obj = JSON.parse(body);
 
-        console.log(obj.Title);
-        console.log(obj.Year);
-        console.log(obj.imdbRating);
-        console.log(obj.tomatoRating);
-        console.log(obj.Country);
-        console.log(obj.Language);
-        console.log(obj.Plot);
-        console.log(obj.Actors);
+//         console.log(obj.Title);
+//         console.log(obj.Year);
+//         console.log(obj.imdbRating);
+//         console.log(obj.tomatoRating);
+//         console.log(obj.Country);
+//         console.log(obj.Language);
+//         console.log(obj.Plot);
+//         console.log(obj.Actors);
 
-    });
+//     });
 
-    if (!movie) {
-        return console.log('Movie not found!');
-    }
+//     if (!movie) {
+//         return console.log('Movie not found!');
+//     }
 
-};
+// };
 
-function doIt() {
+// function doIt() {
 
-    // fs is a core Node package for reading and writing files
-    var fs = require("fs");
+//     // fs is a core Node package for reading and writing files
+//     var fs = require("fs");
 
-    // This block of code will read from the "movies.txt" file.
-    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-    // The code will store the contents of the reading inside the variable "data"
-    fs.readFile("random.txt", "utf8", function (error, data) {
+//     // This block of code will read from the "movies.txt" file.
+//     // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+//     // The code will store the contents of the reading inside the variable "data"
+//     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
-        if (error) {
-            return console.log(error);
-        }
+//         // If the code experiences any errors it will log the error to the console.
+//         if (error) {
+//             return console.log(error);
+//         }
 
-        // We will then print the contents of data
-        //console.log(data);
+//         // We will then print the contents of data
+//         //console.log(data);
 
-        // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
+//         // Then split it by commas (to make it more readable)
+//         var dataArr = data.split(",");
 
-        // We will then re-display the content as an array for later use.
-        console.log(dataArr[0]);
-        console.log(dataArr[1]);
+//         // We will then re-display the content as an array for later use.
+//         console.log(dataArr[0]);
+//         console.log(dataArr[1]);
 
-        //   cannot get the values of dataArr to run in commandline
-        var action = process.argv[2] = dataArr[0];
-        var value = process.argv[3] = dataArr[1];
+//         //   cannot get the values of dataArr to run in commandline
+//         var action = process.argv[2] = dataArr[0];
+//         var value = process.argv[3] = dataArr[1];
 
-    });
+//     });
 
 
-};
+// };
--- test.js
+++ test.js
@@ -1,155 +1,83 @@
-require("dotenv").config();
+require('dotenv').config();
+var Spotify = require('node-spotify-api');
+var keys = require('./keys');
+// var Music = require('./spotify');
+var Movies = require('./movies');
+var axios = require('axios');
 
-// linking to the api key files
-var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var music = new Music();
+var movies = new Movies();
 
-// var spotify = new Spotify(keys.spotify);
+var search = process.argv[2];
+var searchItem = process.argv.slice(3).join(' ');
 
-
-// Load the fs package to read and write
-// var fs = require("fs");
-
-// Take two arguments.
-// The first will be the action (i.e. "deposit", "withdraw", etc.)
-// The second will be the amount that will be added, withdrawn, etc.
-var action = process.argv[2];
-var value = process.argv[3];
-
-//Used to import the client library for the Twitter
-var Twitter = require('twitter');
-
-
-// We will then create a switch-case statement (if-then would also work).
-// The switch-case will direct which function gets run.
-switch (action) {
-    case "my-tweets":
-    tweets();
+switch(search) {
+  case 'concert-this':
+    console.log('Searching concerts ...');
+    music.musician(searchItem);
     break;
-
-    case "spotify-this-song":
-    spotify();
+  case 'spotify-this-song':
+    console.log('Searching songs ...');
+    music.song(searchItem);
     break;
-
-    case "movie-this":
-    movie();
+  case 'movie-this':
+    console.log('Searching movies ...');
+    movies.findMovie(searchItem);
     break;
-    
-    case "do-what-it-says":
-    doIt();
-    break;        
+  case 'do-what-it-says':
+    console.log('Searching for "I Want it That Way"')
+    music.song('I Want it That Way');
+    break;
+  default:
+    console.log('Please enter "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says"');
+    break;
 }
 
-// https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
-// If the "tweets" function is called...
-function tweets() {
+function Music() {
+  this.musician = function(artist) {
+    var URL = 'https//rest.bandsintown.com/artists/' + artist + '/events?app_id=' + spotify;
 
-        var client = new Twitter(keys.twitter);
-      
-        var params = {screen_name: 'pak_attk', count: 20};
-        
-        client.get('statuses/user_timeline', params, function(
-          error,
-          tweets,
-          response
-        ) {
-          if (!error) {
-            var data = []; //empty array to hold data
-            for (var i = 0; i < tweets.length; i++) {
-              data.push({
-                'Date and time of post: ': tweets[i].created_at,
-                'The Tweet: ': tweets[i].text,
-              });
-            }
-            console.log(data);
-          }
-        });
-      };
-    
+    axios
+      .get(URL)
+      .then(function(err, response) {
+        var results = response.data[0];
 
-function spotify() {
+        var artistEvents = `
+      Venue Name: ${results.venue.name}
+      Venue Location: ${results.venue.city}
+      Event Date: ${results.datetime}
+      ----------------------------------------
+      `
 
-    var Spotify = require('node-spotify-api');
- 
-    var spotify = new Spotify(keys.spotify)
- 
-    spotify.search({ type: 'track', query: value, limit: 1 }, function(err, data) {
-    // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        
-        // returning artist name
-        console.log(data.tracks.items[0].artists[0].name);
-
-        // returning song's name 
-        console.log(data.tracks.items[0].name);
-        
-        // returning preview link of the song from Spotify
-        console.log(data.tracks.items[0].preview_url);
-        
-        // returning album that the song is from
-        console.log(data.tracks.items[0].album.name);        
-        });
-    };
-
-function movie() {
-
-    // var omdb = require('omdb');
- 
-    var request = require('request');
-    request('http://www.omdbapi.com/?apikey=trilogy&plot=full&tomatoes=true&r=json&t='+ value, function (error, response, body) {
-    //   console.log('error:', error); // Print the error if one occurred
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-    const obj = JSON.parse(body);
-
-    console.log(obj.Title);
-    console.log(obj.Year);
-    console.log(obj.imdbRating);
-    console.log(obj.tomatoRating);
-    console.log(obj.Country);
-    console.log(obj.Language);
-    console.log(obj.Plot);
-    console.log(obj.Actors);
- 
-    });
- 
-    if(!movie) {
-        return console.log('Movie not found!');
-    }
-    
-};
-
-function doIt() {
-
-    // fs is a core Node package for reading and writing files
-    var fs = require("fs");
-
-    // This block of code will read from the "movies.txt" file.
-    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-    // The code will store the contents of the reading inside the variable "data"
-    fs.readFile("random.txt", "utf8", function(error, data) {
-
-      // If the code experiences any errors it will log the error to the console.
-      if (error) {
-        return console.log(error);
+      if (err) {
+        console.log(err);
       }
 
-      // We will then print the contents of data
-        //console.log(data);
+      console.log(artistEvents);
+      })
+  }
+  
+    this.song = function(songName){
+    spotify.search({ type: 'track', query: songName, limit: 7 }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      } else {
+        var songData = data.tracks.items;
 
-      // Then split it by commas (to make it more readable)
-      var dataArr = data.split(",");
-
-      // We will then re-display the content as an array for later use.
-      console.log(dataArr[0]);
-      console.log(dataArr[1]);
-
-    //   cannot get the values of dataArr to run in commandline
-      var action = process.argv[2] = dataArr[0];
-      var value = process.argv[3] = dataArr[1];
-
+        songData.forEach(function(result) {
+          console.log(`
+      Artist(s): ${songData.artists[0].name}
+      Song Name: ${songData.name}
+      Link: ${songData.preview_url}
+      Album: ${songData.album.name}
+      ---------------------------------------`);
+        });
+      }
+    
+      if(!songName) {
+        songName = 'The Sign';
+      } 
     });
-
-
-};
+  }
+}
