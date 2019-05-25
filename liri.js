ee57d814541926c8aa7d51f23805e2d0f6fe5b7a
AnnabellAnastasia <oav2008@gmail.com>
2019-05-24 23:41:37 -0400

    spotifyBandsOMDB

--- liri.js
+++ liri.js
@@ -1,40 +1,42 @@
 // require the keys from the keys.js folder
 var keys = require('./keys.js');
 
-var Twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
+var dotenv = require("dotenv").config();
 
-var getMyTweets = function () {
+// var Twitter = require('twitter');
+// var spotify = require('spotify');
+// var request = require('request');
 
-    var client = new Twitter(keys.twitterKeys);
-    console.log(keys.twitterKeys);
+// var getMyTweets = function () {
 
-    var params = { screen_name: 'Anna75893882' };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            // console.log(tweets);
-            for (var i = 0; i < tweets.length; i++) {
-                console.log(tweets[i].created_at);
-                console.log('');
-                console.log(tweets[i].text);
-            }
-        }
-    });
-}
+//     var client = new Twitter(keys.twitterKeys);
+//     console.log(keys.twitterKeys);
+
+//     var params = { screen_name: 'Anna75893882' };
+//     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//         if (!error) {
+//             // console.log(tweets);
+//             for (var i = 0; i < tweets.length; i++) {
+//                 console.log(tweets[i].created_at);
+//                 console.log('');
+//                 console.log(tweets[i].text);
+//             }
+//         }
+//     });
+// }
 
 // var getArtistName = function(artist){
 //     return artist.name;
 // }
 
 // var getMeSpotify = function(songName) {
-spotify.search({ type: 'track', query: 'dancing in the moonlight' },
-    function (err, data) {
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
-        console.log(data)
+// spotify.search({ type: 'track', query: 'dancing in the moonlight' },
+//     function (err, data) {
+//         if (err) {
+//             console.log('Error occurred: ' + err);
+//             return;
+//         }
+//         console.log(data)
         //         var songs = data.tracks.items;
         //         for(var i=0; i<songs.length; i++){
         //             console.log(i);
@@ -59,24 +61,24 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' },
 // }
 
 
-var pick = function (caseData, functionData) {
-    switch (caseData) {
-        case 'my-tweets':
-            getMyTweets();
-            break;
-        //         case 'spotify-this-song':
-        //         getMeSpotify(functionData);
-        //         break;
-        //         case 'movie-this':
-        //         getMeMovie(functionData);
-        //         break;
-        default:
-            console.log('Liri does not know that');
-    }
-}
+// var pick = function (caseData, functionData) {
+//     switch (caseData) {
+//         case 'my-tweets':
+//             getMyTweets();
+//             break;
+//         //         case 'spotify-this-song':
+//         //         getMeSpotify(functionData);
+//         //         break;
+//         //         case 'movie-this':
+//         //         getMeMovie(functionData);
+//         //         break;
+//         default:
+//             console.log('Liri does not know that');
+//     }
+// }
 
-var runThis = function (argOne, argTwo) {
-    pick(argOne, argTwo);
-};
+// var runThis = function (argOne, argTwo) {
+//     pick(argOne, argTwo);
+// };
 
-runThis(process.argv[2], process.argv[3]);
+// runThis(process.argv[2], process.argv[3]);
