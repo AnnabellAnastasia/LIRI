5090c12a1d20dca7a3b2ca8dbea06dc81d3af625
AnnabellAnastasia <oav2008@gmail.com>
2019-05-26 14:38:04 -0400

    function liriRun

--- liri.js
+++ liri.js
@@ -1,123 +1,147 @@
 require("dotenv").config();
+
+// Create a variable to access the keys.js file (which is in the same root directory) to access the api keys
+// Create variables for the required package ( node-spotify-api, axios, fs for read/write, and moment)
+
+var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-var keys = require('./keys.js');
-var Movie = require
+var axios = require("axios");
+var fs = require("fs");
+var moment = require('moment');
 
-var pick = function (caseData, functionData) {
-    switch (caseData) {
 
-        case 'concert-this':
-            getMeConcert(functionData);
-            break;
+// Variables for the arguments to be entered by the user in liri
+var appCommand = process.argv[2]
+// console.log("appCommand: " + appCommand);
+// use the slice method to account for user's search starting with index3 position forth because search could have several words
+var userSearch = process.argv.slice(3).join(" ");
 
-        case 'spotify-this-song':
-            getMeSpotify(functionData);
-            break;
+// Using switch statement to execute the code appropriate to the appCommend that is inputed from the user
+function liriRun(appCommand, userSearch) {
+    switch (AppCommand) {
+        case "concert-this":
+            searchForBandsInTown(userSearch);
+            b.slice(3).join(" ");
 
-        case 'movie-this':
-            getMeMovie(functionData);
-            break;
+        // Using switch statement to execute the code appropriate to the appCommend that is inputed from the userk;
+        case "spotify-this-song":
+            getMeSpotify(userSearch);
+            b.slice(3).join(" ");
 
+        // Using switch statement to execute the code appropriate to the appCommend that is inputed from the userk;
+        case "movie-this":
+            getMeMovie(userSearch);
+            b.slice(3).join(" ");
+
+        // Using switch statement to execute the code appropriate to the appCommend that is inputed from the userk;
         case "do-what-it-says":
-            doIt();
+            doWhatItSays();
             break;
-
-        default:
-            console.log('Liri does not know that');
     }
 }
 
-var runThis = function (argOne, argTwo) {
-    pick(argOne, argTwo);
-};
+//pull in required variables
+// var spotifyTest = process.env.SPOTIFY_ID
 
-runThis(process.argv[2], process.argv[3]);
+// fs.appendFile('log.txt', appCommand + ",", function (err) {
+//     if (err) throw err;
+// });
 
+// 
 
-function getMeSpotify() {
+// function searchForBandsInTown(artist) {
+//     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+//     axios.get(queryUrl).then(
+//         function(response) {
+//             if(response.data[0].venue !=  undefined) {
+//                 console.log("Event Veunue: " + response.data[0].venue.name);
+//                 console.log("Event Location: " + response.data[0].venue.city);
+//                 var eventDateTime = moment(response.data[0].datetime);
+//                 console.log("Event Date & Time: " + eventDateTime.format("dddd, MMMM Do YYYY"));
+//             }
+//             else {
+//                 console.log("No results found.");
+//             }
+//         }
+//     ).catch(function (error) {
+//         console.log (error);
+//   });
+// }
 
-    var Spotify = require('node-spotify-api');
+// function getMeSpotify(song) {
+//     spotify
+//         .search({ type: 'track', query: song })
+//         .then(function (response) {
+//             if (response.tracks.total === 0) {
+//                 errorConditionForSpotify();
+//             } else {
+//                 console.log('---------------------------------------------');
+//                 console.log("Artist: " + response.tracks.items[0].artists[0].name);
+//                 console.log("Track: " + response.tracks.items[0].name);
+//                 console.log("Preview URL: " + response.tracks.items[0].preview_url);
+//                 console.log("Album: " + response.tracks.items[0].album.name);
+//                 console.log('---------------------------------------------');
 
-    var spotify = new Spotify(keys.spotify)
+//             }
+//         }).catch(function (error) {
+//             console.log(error);
+//             console.log("No Results found. Showing results for 'The Sign' by Ace of Base");
+//         });
+// }
 
-    spotify.search({ type: 'track', query: value, limit: 1 }, function (err, data) {
-        // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+// function errorConditionForSpotify() {
+//     var spotify = new Spotify(keys.spotify);
 
-        // // returning artist name
-        // console.log(data.tracks.items[0].artists[0].name);
+//     spotify
+//         .search({ type: 'track', query: 'The Sign' })
+//         .then(function (response) {
+//             for (var i = 0; i < response.tracks.items.length; i++) {
+//                 if (response.tracks.items[i].artists[0].name === "Ace of Base") {
+//                     console.log("Artist: " + response.tracks.items[i].artists[0].name);
+//                     console.log("Track: " + response.tracks.items[i].name);
+//                     console.log("Preview URL: " + response.tracks.items[i].preview_url);
+//                     console.log("Album: " + response.tracks.items[i].album.name);
+//                     i = response.tracks.items.length;
+//                 }
+//             }
+//         }).catch(function (error) {
+//             console.log(error);
+//             console.log("No Results found. ");
+//         });
+// }
 
-        // // returning song's name 
-        // console.log(data.tracks.items[0].name);
-
-        // // returning preview link of the song from Spotify
-        // console.log(data.tracks.items[0].preview_url);
-
-        // // returning album that the song is from
-        // console.log(data.tracks.items[0].album.name);
-    });
-};
-
-// function getMeMovie() {
-
-//     // var omdb = require('omdb');
-
-//     var request = require('request');
-//     request('http://www.omdbapi.com/?apikey=trilogy&plot=full&tomatoes=true&r=json&t=' + value, function (error, response, body) {
-//         //   console.log('error:', error); // Print the error if one occurred
-//         //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-//         const obj = JSON.parse(body);
-
-//         console.log(obj.Title);
-//         console.log(obj.Year);
-//         console.log(obj.imdbRating);
-//         console.log(obj.tomatoRating);
-//         console.log(obj.Country);
-//         console.log(obj.Language);
-//         console.log(obj.Plot);
-//         console.log(obj.Actors);
-
+// function getMeMovie(movieName) {
+//     axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
+//         function (response) {
+//             //console.log(response.data);
+//             if (response.data.Title != undefined) {
+//                 console.log("Title: " + response.data.Title);
+//                 console.log("Year: " + response.data.Year);
+//                 console.log("imdbRating:: " + response.data.imdbRating);
+//                 console.log("RottenTomatoes: " + response.data.tomatoRating);
+//                 console.log("Country:: " + response.data.Country);
+//                 console.log("Language:: " + response.data.Language);
+//                 console.log("Plot: " + response.data.Plot);
+//                 console.log("Actors: " + response.data.Actors);
+//             }
+//             else {
+//                 getMeMovie("Mr. Nobody");
+//             }
+//         }
+//         // if response is empty call the api again with the "default" movie 
+//     ).catch(function (error) {
+//         console.log(error);
+//         console.log("No Results found. ");
 //     });
+// }
 
-//     if (!movie) {
-//         return console.log('Movie not found!');
-//     }
-
-// };
-
-// function doIt() {
-
-//     // fs is a core Node package for reading and writing files
-//     var fs = require("fs");
-
-//     // This block of code will read from the "movies.txt" file.
-//     // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-//     // The code will store the contents of the reading inside the variable "data"
+// function doWhatItSays() {
 //     fs.readFile("random.txt", "utf8", function (error, data) {
-
+//         var dataArr = data.split(",");
+//         getMeSpotify(dataArr[1])
 //         // If the code experiences any errors it will log the error to the console.
 //         if (error) {
 //             return console.log(error);
 //         }
-
-//         // We will then print the contents of data
-//         //console.log(data);
-
-//         // Then split it by commas (to make it more readable)
-//         var dataArr = data.split(",");
-
-//         // We will then re-display the content as an array for later use.
-//         console.log(dataArr[0]);
-//         console.log(dataArr[1]);
-
-//         //   cannot get the values of dataArr to run in commandline
-//         var action = process.argv[2] = dataArr[0];
-//         var value = process.argv[3] = dataArr[1];
-
 //     });
-
-
-// };
+// }
