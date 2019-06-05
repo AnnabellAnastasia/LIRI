0f5436ca8facc9dd9b9fd5d410badd1cae458089
AnnabellAnastasia <oav2008@gmail.com>
2019-06-05 11:06:24 -0400

    readme

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 
-// console.log('this is loaded');
+console.log('this is loaded');
 
-// exports.spotify = {
-//   id:'1cae67c2ee9448a381f2e609b67c2fc9',
-//   secret:'468dadf8014d4cfdb5bce5cfc5d457fc'
-// };
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
